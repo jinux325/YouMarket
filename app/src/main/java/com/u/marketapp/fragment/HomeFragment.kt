267691f63d76e3962f333a3880ee658376ab58e9
kangmingu93 <35962595+kangmingu93@users.noreply.github.com>
2020-03-25 22:08:12 +0900

    글쓰기 페이지 내용 줄바꿈 키 수정

--- app/src/main/java/com/u/marketapp/fragment/HomeFragment.kt
+++ app/src/main/java/com/u/marketapp/fragment/HomeFragment.kt
@@ -5,7 +5,6 @@ import android.app.Activity
 import android.app.AlertDialog
 import android.content.Context
 import android.content.Intent
-import android.content.SharedPreferences
 import android.os.Bundle
 import android.preference.PreferenceManager
 import android.util.Log
