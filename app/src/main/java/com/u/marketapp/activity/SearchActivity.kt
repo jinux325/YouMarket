68f5c3b3856513550b5de29992f4dc51a898b490
kangmingu93 <35962595+kangmingu93@users.noreply.github.com>
2020-04-12 21:53:22 +0900

    검색 화면 추가, 거래 상태 표시

---
+++ app/src/main/java/com/u/marketapp/activity/SearchActivity.kt
@@ -0,0 +1,13 @@
+package com.u.marketapp.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.u.marketapp.R
+
+class SearchActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search)
+    }
+}
--- app/src/main/java/com/u/marketapp/adapter/BindingAdapter.kt
+++ app/src/main/java/com/u/marketapp/adapter/BindingAdapter.kt
@@ -1,5 +1,6 @@
 package com.u.marketapp.adapter
 
+import android.graphics.Color
 import android.view.View
 import android.widget.ImageView
 import android.widget.LinearLayout
@@ -91,6 +92,27 @@ fun bindViewFromSuggestion(view: MaterialTextView, suggestion: Boolean) {
     } else {
         view.text = view.resources.getString(R.string.product_unable_to_offer_price)
         view.setTextColor(ContextCompat.getColor(view.context, R.color.colorGray))
+    }
+}
+
+@BindingAdapter("bindTrade")
+fun bindViewFromTrade(view: MaterialTextView, trade: Int) {
+    when (trade) {
+        0 -> { // 거래중
+            view.visibility = View.GONE
+        }
+        1 -> { // 예약중
+            view.visibility = View.VISIBLE
+            view.text = view.resources.getText(R.string.trade_1)
+            view.setBackgroundColor(ContextCompat.getColor(view.context, R.color.colorPrimary))
+            view.setTextColor(ContextCompat.getColor(view.context, R.color.white))
+        }
+        2 -> { // 거래완료
+            view.visibility = View.VISIBLE
+            view.text = view.resources.getText(R.string.trade_2)
+            view.setBackgroundColor(ContextCompat.getColor(view.context, R.color.account_addressColor))
+            view.setTextColor(ContextCompat.getColor(view.context, R.color.white))
+        }
     }
 }
 
